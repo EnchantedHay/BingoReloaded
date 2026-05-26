a0f55c5dd94fe2e4859880dbb68cf8be3481533f
ChanceLetHay <17094287+chancelethay@user.noreply.gitee.com>
2026-05-26 14:53:03 +0800

    Refactor to Paper-only plugin

--- BingoReloadedCompanion/src/client/java/io/github/steaf23/bingoreloadedcompanion/client/hud/HudConfig.java
+++
@@ -1,9 +0,0 @@
-package io.github.steaf23.bingoreloadedcompanion.client.hud;
-
-import net.minecraft.resources.Identifier;
-
-import java.util.Map;
-
-public record HudConfig(String version, Map<Identifier, HudPlacement> elements) {
-
-}
--- BingoReloadedCompanion/src/client/java/io/github/steaf23/bingoreloadedcompanion/client/hud/HudInfo.java
+++
@@ -1,4 +0,0 @@
-package io.github.steaf23.bingoreloadedcompanion.client.hud;
-
-public record HudInfo(boolean resizable, int minSizeX, int minSizeY) {
-}
